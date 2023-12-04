6038df0fdd275b0a3c31a39d8240385c4f39aa0b
Franziska-09 <franziska.gschweidl@stud.fh-campuswien.ac.at>
2023-12-04 20:33:33 +0100

    integration of the MaterialFX stylesheet part -> fixed the visability problems with the checkboxes and the general style problems

--- src/main/java/at/ac/fhcampuswien/synapsenchat/HelloApplication.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/HelloApplication.java
@@ -2,22 +2,38 @@ package at.ac.fhcampuswien.synapsenchat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
+
+
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+     //   scene.getStylesheets().add(getClass().getResource("").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
