4dde76ea4a8b6ddde4d7329d036928a2ad9a5f2a
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2023-12-12 20:13:31 +0100

    Simple ClientConnection with CLI Input for IP, Port & Message

--- src/main/java/at/ac/fhcampuswien/synapsenchat/connection/client/Client.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/connection/client/Client.java
@@ -1,4 +1,5 @@
 package at.ac.fhcampuswien.synapsenchat.connection.client;
+
 import java.net.*;
 import java.io.*;
 
@@ -6,15 +7,48 @@ public class Client {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+    public void startConnection(String ip, int port) {
+
+        // while(true){
+        try {
+            clientSocket = new Socket(ip, port);
+
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("Connection established");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //   receiveMessage();
+        // }
+
     }
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
+
+    public void stopConnection() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void sendMessage(String msg) {
+        out.println(msg);
+    }
+
+    public void receiveMessage() {
+        try {
+            System.out.println(in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/main/java/at/ac/fhcampuswien/synapsenchat/connection/client/ClientHandler.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/connection/client/ClientHandler.java
@@ -1,13 +1,32 @@
 package at.ac.fhcampuswien.synapsenchat.connection.client;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ClientHandler {
-    public static void main(String[] args) throws IOException {
-        int port = 12322;
-        String ip = "127.0.0.1";
+    public static void main(String[] args){
 
-        Client client = new Client(ip, port);
-        client.sendMessage(client.getSocket(), "TESTING!");
+        int port;
+        String ip;
+        String msg;
+
+        Scanner scanner = new Scanner(System.in);
+
+        Client client = new Client();
+
+        System.out.println("Inport the server IP you want to connect: ");
+        ip = scanner.nextLine();
+
+        System.out.println("Input your Port for Connection: ");
+        port = scanner.nextInt();
+
+        client.startConnection(ip,port);
+
+        System.out.println("Input Message(Enter to send): ");
+        msg = scanner.nextLine();
+        client.sendMessage(msg);
+
+        client.receiveMessage();
     }
+
 }
