75fa73b178d27ace61874bf8c0f347cbc81333f2
Franziska-09 <franziska.gschweidl@stud.fh-campuswien.ac.at>
2023-12-29 09:19:04 +0100

    adding of new fxml files for the chat creation and the chat content + some logic of Center switching

--- src/main/java/at/ac/fhcampuswien/synapsenchat/HelloApplication.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import io.github.palexdev.materialfx.theming.JavaFXThemes;
@@ -28,7 +29,7 @@ public class HelloApplication extends Application {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
      //   scene.getStylesheets().add(getClass().getResource("").toExternalForm());
-        stage.setTitle("Hello!");
+        stage.setTitle("Synapsenchat");
         stage.setScene(scene);
         scene.getStylesheets().add(HelloApplication.class.getResource("styles.css").toExternalForm());
         stage.show();
--- src/main/java/at/ac/fhcampuswien/synapsenchat/HelloController.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/HelloController.java
@@ -1,26 +1,69 @@
 package at.ac.fhcampuswien.synapsenchat;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXRadioButton;
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
 
     @FXML
-    MFXScrollPane chatContent;
-    @FXML
-    MFXScrollPane newChat;
+    BorderPane newChat;
     @FXML
     Label startLabel;
+    @FXML
+    VBox chatContent;
+    @FXML
+    protected BorderPane homePane;
+    @FXML
+    protected AnchorPane view;
+    @FXML
+    MFXButton startConnection;
 
     @FXML
-    protected void onNewChatButtonClick(){
+    protected void onNewChatButtonClick() {
         System.out.println("New chat!");
-        if(chatContent.getContent().isVisible())
+        if (chatContent.isVisible())
             chatContent.setVisible(false);
-        if(startLabel.isVisible())
+        if (startLabel.isVisible())
             startLabel.setVisible(false);
         newChat.setVisible(true);
     }
+    @FXML
+    protected void showChatContent(ActionEvent event) throws IOException {
+        view = FXMLLoader.load(getClass().getResource("chatContent.fxml"));
+        BorderPane chatPane = (BorderPane) startConnection.getScene().getRoot();
+        chatPane.setCenter(view);
+
+    }
+
+    @FXML
+    protected void showNewChat(ActionEvent event) throws IOException{
+        view = FXMLLoader.load(getClass().getResource("newChat.fxml"));
+        homePane.setCenter(view);
+    }
+
+    @FXML
+    protected void onStartConnectionButtonClick(){
+        if (startLabel.isVisible())
+            startLabel.setVisible(false);
+        if (newChat.isVisible())
+            newChat.setVisible(false);
+//        chatContent.setVisible(true);
+    }
+
+
 }
