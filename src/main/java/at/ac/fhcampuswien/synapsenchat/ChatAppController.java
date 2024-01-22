31574565590164c8b24b973a6748cc76726753cc
Stephan Bollauf <stephan.bollauf@gmail.com>
2024-01-22 17:03:51 +0100

    change color

--- src/main/java/at/ac/fhcampuswien/synapsenchat/ChatAppController.java
+++ src/main/java/at/ac/fhcampuswien/synapsenchat/ChatAppController.java
@@ -15,7 +15,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.Inet4Address;
